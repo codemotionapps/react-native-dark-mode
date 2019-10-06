5246ca8cb594235c90faf2188608e72de1040958
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-10-06 04:23:11 +0300

    Closes #23

--- library/android/src/main/java/com/codemotionapps/reactnativedarkmode/DarkModeModule.java
+++ library/android/src/main/java/com/codemotionapps/reactnativedarkmode/DarkModeModule.java
@@ -1,29 +1,47 @@
 package com.codemotionapps.reactnativedarkmode;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.Build;
 
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class DarkModeModule extends ReactContextBaseJavaModule {
+public class DarkModeModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 	public static ReactApplicationContext reactContext;
 
 	public DarkModeModule(final ReactApplicationContext reactContext) {
 		super(reactContext);
 		this.reactContext = reactContext;
 
-		Intent intent = new Intent(reactContext, DarkModeService.class);
-		// https://stackoverflow.com/a/47654126/882847
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-			reactContext.getApplicationContext().startForegroundService(intent);
-		} else {
+		reactContext.addLifecycleEventListener(this);
+
+		if (isAppOnForeground(reactContext)) {
+			Intent intent = new Intent(reactContext, DarkModeService.class);
 			reactContext.getApplicationContext().startService(intent);
 		}
+	}
+
+	private boolean isAppOnForeground(Context context) {
+		ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+		if (appProcesses == null) {
+			return false;
+		}
+		final String packageName = context.getPackageName();
+		for (RunningAppProcessInfo appProcess : appProcesses) {
+			if (appProcess.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND && appProcess.processName.equals(packageName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -44,5 +62,23 @@ public class DarkModeModule extends ReactContextBaseJavaModule {
 		return (configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
 				? "dark"
 				: "light";
+	}
+
+	@Override
+	public void onHostResume() {
+		Intent intent = new Intent(reactContext, DarkModeService.class);
+		reactContext.getApplicationContext().startService(intent);
+	}
+
+	@Override
+	public void onHostPause() {
+		Intent intent = new Intent(reactContext, DarkModeService.class);
+		reactContext.getApplicationContext().stopService(intent);
+	}
+
+	@Override
+	public void onHostDestroy() {
+		Intent intent = new Intent(reactContext, DarkModeService.class);
+		reactContext.getApplicationContext().stopService(intent);
 	}
 }
