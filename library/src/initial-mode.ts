19b2503c20f8e50dfa40e44cf464e99b80a87fc2
Corbin Crutchley <crutchcorn@gmail.com>
2020-05-28 17:40:30 -0700

    Make dark mode library initially support web

--- library/src/initial-mode.ts
+++ library/src/initial-mode.ts
@@ -1,5 +1,5 @@
 import { NativeModule } from './native-module'
 import { Mode } from './types'
 
-export const initialMode: Mode = NativeModule.initialMode
-export const supportsDarkMode: boolean = NativeModule.supportsDarkMode
+export const initialMode: Mode = (NativeModule && NativeModule.initialMode) || 'light'
+export const supportsDarkMode: boolean = (NativeModule && NativeModule.supportsDarkMode) || false
